065ca7baf883c0596594d7c2c566e488a40d97f3
emendoza <emendoza@DESKTOP-OMUQ8KR>
2020-11-24 23:11:39 -0500

    Add project files.

---
+++ OnBoardingProyecto/OnBoardingProyecto/InsertarDato.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Net;
+
+namespace OnBoardingProyecto
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InsertarDato : ContentPage
+    {
+        public InsertarDato()
+        {
+            InitializeComponent();
+        }
+
+        private async  void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo",txtCodigo.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+                cliente.UploadValues("http://192.168.100.27/onboarding/post.php","POST",parametros);
+                await DisplayAlert("Alerta", "Dato ingresado correctamente", "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Alerta", "Error:" + ex.Message, "OK");
+
+            }
+
+        }
+
+        private async void btnRegresar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
---
+++ OnBoardingProyecto/OnBoardingProyecto/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OnBoardingProyecto
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "http://192.168.100.27/onboarding/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<OnBoardingProyecto.Ws.Datos> _post;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            get();
+        }
+
+        public async void get(){
+            try
+            {
+                var content = await client.GetStringAsync(Url);
+                List<OnBoardingProyecto.Ws.Datos> posts = JsonConvert.DeserializeObject<List<OnBoardingProyecto.Ws.Datos>>(content);
+                _post = new ObservableCollection<OnBoardingProyecto.Ws.Datos>(posts);
+                MyListView.ItemsSource = _post;
+            }
+            catch (Exception ex){
+                DisplayAlert("Error", "ERROR"+ex.Message,"OK");
+            }
+        }
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new InsertarDato());
+
+        }
+        private void btnPost_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnDelete_Clicked(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new Delete());
+        }
+
+
+    }
+}
